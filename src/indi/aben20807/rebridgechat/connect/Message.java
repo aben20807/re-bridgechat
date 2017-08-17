85ba02a34e1161eed7c696b58ef3cf169c8327b2
aben20807 <aben20807@gmail.com>
2017-08-17 22:07:44 +0800

    Add Message

---
+++ src/indi/aben20807/rebridgechat/connect/Message.java
@@ -0,0 +1,9 @@
+package indi.aben20807.rebridgechat.connect;
+
+import java.io.Serializable;
+
+public class Message implements Serializable{
+
+	private static final long serialVersionUID = 1371494515691412295L;
+
+}
