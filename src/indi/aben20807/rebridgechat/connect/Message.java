1cb1720c9d9b6c70d9221d73dffb674f0a371e75
aben20807 <aben20807@gmail.com>
2017-08-29 11:10:41 +0800

    Let Message can conatin different type

--- src/indi/aben20807/rebridgechat/connect/Message.java
+++ src/indi/aben20807/rebridgechat/connect/Message.java
@@ -2,10 +2,30 @@ package indi.aben20807.rebridgechat.connect;
 
 import java.io.Serializable;
 
+import indi.aben20807.rebridgechat.bridge.Card;
+
 public class Message implements Serializable{
 
 	private static final long serialVersionUID = 1371494515691412295L;
 	private String content;
+	private Card card;
+	private MessageOption option;
+	
+	public Message(Object object, MessageOption option) {
+		setOption(option);
+		switch (option) {
+			case CHAT:
+			case CALL:
+			case COMMAND:
+				setContent((String) object);
+				break;
+			case CARD:
+				setCard((Card) object);
+				break;
+			default:
+				break;
+		}
+	}
 	
 	public Message(String content) {
 		setContent(content);
@@ -22,5 +42,21 @@ public class Message implements Serializable{
 	
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public MessageOption getOption() {
+		return option;
+	}
+
+	public void setOption(MessageOption option) {
+		this.option = option;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
 	}
 }
---
+++ src/indi/aben20807/rebridgechat/connect/MessageOption.java
@@ -0,0 +1,6 @@
+package indi.aben20807.rebridgechat.connect;
+
+public enum MessageOption {
+
+	CARD, CHAT, CALL, COMMAND;
+}
