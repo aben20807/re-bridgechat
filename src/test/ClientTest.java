f4d6b60b7475f0ae326c495dddf40b15fbd0535a
aben20807 <aben20807@gmail.com>
2017-08-30 23:09:13 +0800

    ClientTest can loop to submit message

--- src/test/ClientTest.java
+++ src/test/ClientTest.java
@@ -1,6 +1,11 @@
 package test;
 
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import indi.aben20807.rebridgechat.bridge.Card;
 import indi.aben20807.rebridgechat.bridge.Suits;
@@ -11,6 +16,7 @@ import indi.aben20807.rebridgechat.connect.client.Client;
 public class ClientTest {
 
 	private static Scanner scanner = new Scanner(System.in);
+	final static ExecutorService executorService = Executors.newCachedThreadPool();
 	
 	public static void main(String[] args) {
 
@@ -18,7 +24,16 @@ public class ClientTest {
 		String s;
 		do{
 			s = scanner.nextLine();
-			client.submitToServer(new Message(new Card('T', Suits.CLUBS, 10), MessageOption.CARD));
-		}while(!s.equals("bye"));
+			switch (s) {
+				case "chat":
+					client.submitToServer(new Message(scanner.nextLine()));
+					break;
+				case "card":
+					client.submitToServer(new Message(new Card('T', Suits.CLUBS, 10), MessageOption.CARD));
+					break;
+				default:
+					break;
+			}
+		}while(!s.equals("exit"));
 	}
 }
